2667acb2a37af5c8e370b47e2d7d62863fc640f7
Manikandan <manicaresin@gmail.com>
2024-10-30 11:58:34 +0530

    01

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+console.log('Manikandan ' +
+    'Who am i? ' +
+    'Alive or not? ' +
+    'Done with the world? ' +
+    'youtube.com @enjoykit ' +
+    'https://enjoykit.com/'
+
+);
